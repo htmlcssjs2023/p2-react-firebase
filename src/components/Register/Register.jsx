4ba2525ca47ad2894eddae1001d522239793b051
mdabdulhalim <htmlcssjs2023@gmail.com>
2023-10-16 07:12:18 +0600

    create firebase user using email and password

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,58 +1,33 @@
 import React, { useState } from 'react';
 import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
-import app from '../../firebase/firebase.init';
+import app from '../../firebase/firebase.config';
 
 const auth = getAuth(app);
 
 const Register = () => {
-        const [error, setError] = useState('');
-        const [success, setSuccess] = useState('');
-    const handleSubmit = (event)=>{
-        
-        // 1. Prevent Page refresh
+    const handleSubmit = (event) => {
+        console.log(event.target);
+        // 1. prevent page refresh
         event.preventDefault();
-
-        // Collect form data
-        const name = event.target.name.value;
+        
+        // collect form data
+        const fullName = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(name, email, password);
+        console.log(fullName, email, password);
 
-            // form input formate validation
-        if(/(?=.*[A-Z])/.test(password)){
-            setError('Please add at least one uppercase');
-            return;
-        }
-        else if(/(?=.*[0-9].*[0-9])/){
-            setError('Please add at least number');
-            return;
-        }
-        else if(password.length < 6){
-            setError('Password must be at least six character');
-            return;
-        }
-
-    // create user 
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCaredential)=>{
-        const user = userCaredential.user;
-        console.log(user);
-    })
-    .catch(error =>{
-        console.log(error.message);
-    })
-
-    // sign in user with email and password
-    signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential)=>{
-        const user = userCredential.user;
-        console.log(user);
-    })
-    .catch(error =>{
-        console.log(error.message);
-    })
+        // create user in firebase
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            console.log(loggedUser);
+        })
+        .catch( error => {
+            console.error(error.message);
+        })
 
     }
+        
     return (
         <div className='w-50 mx-auto'>
         <h2>Please Login</h2>
@@ -65,13 +40,10 @@ const Register = () => {
             <div className="form-group mb-3">
                 <label htmlFor="email">Email address</label>
                 <input type="email" name='email' className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>
-               
-
             </div>
             <div className="form-group mb-3">
                 <label htmlFor="password">Password</label>
                 <input type="password" name='password' className="form-control" id="password" placeholder="Password"/>
-            
             </div>
             <div className="form-check mb-3">
                 <input type="checkbox" className="form-check-input" id="rememberMe" />
@@ -79,7 +51,7 @@ const Register = () => {
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
-        <p className='text-danger'>{error}</p>
+        
        
     </div>
     );
